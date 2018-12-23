4d0897698fa361afa41365404cfbbd98586546a1
Chris <psycpk@exmail.nottingham.ac.uk>
2018-12-23 21:47:41 +0000

    Added camera controller

--- Assets/Scripts/BoidSpawner.cs
+++ Assets/Scripts/BoidSpawner.cs
@@ -16,6 +16,12 @@ public class BoidSpawner : MonoBehaviour {
   
         SpawnBoids(numBoids);
 	}
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawCube(transform.position, new Vector3(0.2f, 0.2f, 0.2f));
+    }
 	
 	// Update is called once per frame
 	void Update () {
